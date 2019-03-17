a000e1a56bacc9949a1fae0c66fdded88ae8324b
Petr Lukashev <lukashev_pjer@mail.ru>
2019-03-17 23:09:17 +0300

    added spring, my batis and repository

---
+++ src/main/java/lukashev/petshoptesttask/Main.java
@@ -0,0 +1,39 @@
+package lukashev.petshoptesttask;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Main {
+
+    private static Repository repository= new Repository();
+
+    public static void main(String[] args) {
+
+      /*  System.out.println(repository.getPersonById(1));
+        System.out.println("//////////////////");
+        System.out.println(repository.getAllPersons());
+        System.out.println("//////////////////");
+        System.out.println(repository.deletePersonById(2));
+        System.out.println("//////////////////");
+        System.out.println(repository.getCarById(4));
+        System.out.println("//////////////////");
+        System.out.println(repository.getAllCars());
+        System.out.println("//////////////////");
+        System.out.println(repository.deleteCarById(5));*/
+/*
+      Person person = new Person(11,"Vyacheslav", "someemail@yahoo.com", null);
+      Person person1 = new Person(3,"Vyac", "someemail@yahoo.com", null);
+        System.out.println(repository.deletePersonById(2));
+        System.out.println(repository.createPerson(person));
+        System.out.println(repository.updatePerson(person1));
+
+      System.out.println(repository.createCar(new Car(35,"lambogini", 350, person)));*/
+
+     Person person2 = repository.getPersonById(2);
+
+        System.out.println(repository.updateCar(new Car(8, "hyundai", 180, person2)));
+
+    }
+}
---
+++ src/main/java/lukashev/petshoptesttask/Repository.java
@@ -0,0 +1,130 @@
+package lukashev.petshoptesttask;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+
+@org.springframework.stereotype.Repository
+public class Repository {
+
+   private SqlSessionFactory sqlSessionFactory;
+
+   private Reader reader;
+
+public Repository(){
+    try {
+        reader = Resources
+                .getResourceAsReader("db/mybatis-config.xml");
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+
+public Person getPersonById(int id){
+    SqlSession session = sqlSessionFactory.openSession();
+    try {
+        return session.selectOne("lukashev.petshoptesttask.PersonMapper.getPersonById", id);
+    } finally {
+        session.close();
+    }
+}
+
+public List<Person> getAllPersons(){
+    SqlSession session = sqlSessionFactory.openSession();
+    try {
+      return session.selectList("lukashev.petshoptesttask.PersonMapper.getAllPersons");
+    } finally {
+        session.close();
+    }
+}
+
+public boolean updatePerson(Person person){
+    SqlSession session = sqlSessionFactory.openSession();
+    try {
+        int result = session.update("lukashev.petshoptesttask.PersonMapper.updatePerson",person);
+        session.commit();
+        return  result>0;
+    } finally {
+        session.close();
+    }
+}
+
+    public boolean createPerson(Person person){
+    SqlSession session = sqlSessionFactory.openSession();
+        try {
+            int result = session.insert("lukashev.petshoptesttask.PersonMapper.insertPerson",person);
+            session.commit();
+            return  result>0;
+        } finally {
+            session.close();
+        }
+}
+
+    public boolean deletePersonById(int id){
+    SqlSession session = sqlSessionFactory.openSession();
+        try {
+            int result = session.delete("lukashev.petshoptesttask.PersonMapper.deletePersonById", id);
+            session.commit();
+            return  result>0;
+        } finally {
+            session.close();
+        }
+}
+
+    public Car getCarById(int id){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            return session.selectOne("lukashev.petshoptesttask.CarMapper.getCarById", id);
+        } finally {
+            session.close();
+        }
+    }
+
+    public List<Car> getAllCars(){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            return  session.selectList("lukashev.petshoptesttask.CarMapper.getAllCars");
+        } finally {
+            session.close();
+        }
+    }
+
+    public boolean updateCar(Car car){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            int result = session.update("lukashev.petshoptesttask.CarMapper.updateCar", car);
+            session.commit();
+            return  result>0;
+        } finally {
+            session.close();
+        }
+    }
+
+    public boolean createCar(Car car){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            int result = session.insert("lukashev.petshoptesttask.CarMapper.insertCar", car);
+            session.commit();
+            return  result>0;
+        } finally {
+            session.close();
+        }
+    }
+
+    public boolean deleteCarById(int id){
+        SqlSession session = sqlSessionFactory.openSession();
+        try {
+            int result = session.delete("lukashev.petshoptesttask.CarMapper.deleteCarById", id);
+            session.commit();
+            return  result>0;
+        } finally {
+            session.close();
+        }
+    }
+}
